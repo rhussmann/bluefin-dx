7fdbc5abd919d3302374957bd209fe62d6896ccc
Ricky Hussmann <ricky.hussmann@gmail.com>
2025-03-03 23:43:43 -0500

    feat: add 1Password dependencies

--- build.sh
+++ build.sh
@@ -24,6 +24,12 @@ dnf5 -y install system76-driver
 dnf5 -y install firmware-manager system76-power system76-dkms
 dnf5 -y copr disable szydell/system76
 
+# Setup 1Password repos
+
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+dnf5 -y install 1password 1password-cli
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
