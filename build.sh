7d84d11c256adb4076b158a7376f03dd64bac394
Ricky Hussmann <ricky.hussmann@gmail.com>
2025-03-04 00:13:25 -0500

    trying rpm-ostree

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ dnf5 -y copr disable szydell/system76
 
 rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-dnf install -y 1password 1password-cli
+rpm-ostree install 1password 1password-cli
 
 #### Example for enabling a System Unit File
 
